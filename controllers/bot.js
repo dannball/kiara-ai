3764f8515270b959320dc5a0fffc414918414b96
xbal023 <iqbalsheila619@gmail.com>
2025-07-02 01:43:40 +0700

    feat: add auth middleware

--- controllers/bot.js
+++ controllers/bot.js
@@ -1,11 +1,10 @@
 const { System } = require('../database');
 const bot = require('../lib/bot');
+const authMiddleware = require('../lib/middlewares/auth');
 
 module.exports = async (req, res, next) => {
     try {
         let { params, body } = req;
-        // let apikey = req.headers['x-api-key'];
-        // if (!apikey) return next();
         if (!body) return next();
         let command = `${params.type}:${params.cmd}`;
         let data = null;
@@ -13,14 +12,16 @@ module.exports = async (req, res, next) => {
         switch (command) {
 
             case 'bot:create': {
+                await authMiddleware(req, ['dev']);
                 let { botNumber, systemName } = body;
                 if (!systemName) throw new Error("nama Sistem diperlukan!");
                 if (!botNumber) throw new Error("bot number diperlukan!");
                 const result = await System.create({ botNumber, name: systemName });
                 data = result.id;
             } break;
-
+            
             case 'bot:start': {
+                await authMiddleware(req, ['dev']);
                 let { systemId, override } = body;
                 let result = await System.findOne({ where: { id: systemId }});
                 if (!result.botNumber) throw new Error("Bot number masih kosong!");
@@ -28,30 +29,34 @@ module.exports = async (req, res, next) => {
                 result = await bot(result.id, true);
                 data = result;
             } break;
-
+            
             case 'bot:getpaircode': {
+                await authMiddleware(req, ['dev']);
                 let { systemId } = body;
                 let result = await System.findOne({ where: { id: systemId }});
                 if (!result) throw new Error("Bot tidak ditemukan! (404)")
-                if (!result.botNumber) throw new Error("Bot number masih kosong!");
+                    if (!result.botNumber) throw new Error("Bot number masih kosong!");
                 if (result.status == 'siap') throw new Error("Pairing code tidak tersedia dengan status\"siap\"");
                 result = result.pairCode && !["turu", "ban", "berak"].includes(result.status) ? result.toJSON() : await bot(result.id);
                 data = result;
             } break;
-
+            
             case 'bot:getstatus': {
+                await authMiddleware(req, ['dev']);
                 let { systemId } = body;
                 const result = await System.findOne({ where: { id: systemId }}).then(s => s.toJSON());
                 data = result;
             } break;
 
             case 'bot:delete': {
+                await authMiddleware(req, ['dev']);
                 let { systemId } = body;
                 const result = await System.destroy({ where: { id: systemId }});
                 data = result;
             } break;
-
+            
             case 'bot:update': {
+                await authMiddleware(req, ['dev']);
                 let { systemId, botNumber, systemName } = body;
                 const result = await System.update({ 
                     ...(botNumber ? {botNumber} : {}), 
